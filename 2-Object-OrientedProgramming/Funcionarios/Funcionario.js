449fef7b9dd764fc1288069681690489a689bf83
estevaoreis25 <estevaoreis1587@gmail.com>
2021-06-10 16:33:37 -0300

    Reorganizing files and creating three new classes

---
+++ 2-Object-OrientedProgramming/Funcionarios/Funcionario.js
@@ -0,0 +1,7 @@
+export class Funcionario {
+    constructor(nome, cpf, salario){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+}
--- 2-Object-OrientedProgramming/index.js
+++ 2-Object-OrientedProgramming/index.js
@@ -1,7 +1,7 @@
 import {Cliente} from "./Cliente.js";
-import { Conta } from "./Conta.js";
-import {ContaCorrente} from "./ContaCorrente.js";
-import {ContaPoupanca} from "./ContaPoupanca.js";
+import {ContaCorrente} from "./Conta/ContaCorrente.js";
+import {ContaPoupanca} from "./Conta/ContaPoupanca.js";
+import {ContaSalario} from "./Conta/ContaSalarios.js";
 
 const cliente1 = new Cliente("Diego", 397463819201);
 
@@ -10,8 +10,10 @@ const contaCorrenteDiego = new ContaCorrente(1001, cliente1, 30);
 contaCorrenteDiego.depositar(90);
 
 const contaPoupancaDiego = new ContaPoupanca(50, cliente1, 1001);
+const contaSalarioDiego = new ContaSalario(cliente1, 20021);
 
 console.log(contaCorrenteDiego);
 console.log(contaPoupancaDiego);
+console.log(contaSalarioDiego);
 
 console.log(`Numero de contas correntes no momento ${ContaCorrente.numeroContas}`);
